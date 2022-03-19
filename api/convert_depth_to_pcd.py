ef27353e38bdca94ee2b417248e51c7435540f95
Alex Liu <alex.liuyining@outlook.com>
2022-03-19 15:31:13 -0400

    Refactored depth image to pcd code

--- api/convert_depth_to_pcd.py
+++ api/convert_depth_to_pcd.py
@@ -1,20 +1,11 @@
-from cv2 import IMREAD_ANYDEPTH, IMREAD_UNCHANGED
 import numpy as np
+import open3d as o3d
+
 import os
 import sys
-import open3d as o3d
-import cv2
+import argparse
 
-# depth_unit: 0.03125
-# width: 1280
-# height: 1024
-# fx: 1083.097046
-# fy: 1083.097046
-# cx: 379.326874
-# cy: 509.437195
-# baseline: 101.1839
-# disparity_shift: 0
-
+# ROBI parameters
 w = 1280
 h = 1024
 fx = 1083.097046
@@ -22,22 +13,13 @@ fy = 1083.097046
 cx = 379.326874
 cy = 509.437195
 
+# depth2depth sample input parameters
 # w = 256
 # h = 144
 # fx = 185
 # fy = 185
 # cx = 128
 # cy = 72
-
-
-def scale_depth_image(depth_img_path, out_path, scale=32.0):
-    depth_img = cv2.imread(depth_img_path, IMREAD_ANYDEPTH)
-    # depth_img = np.true_divide(depth_img, scale)
-    # depth_img = np.multiply(depth_img, 32)
-    depth_img = depth_img.astype(np.uint16)
-    print(np.min(depth_img), np.max(depth_img))
-    cv2.imwrite(out_path, depth_img)
-
 
 def create_point_cloud(depth_img_path, scale=1000.0):
     depth_img = o3d.io.read_image(depth_img_path)
@@ -56,11 +38,13 @@ def create_point_cloud(depth_img_path, scale=1000.0):
 
     o3d.visualization.draw_geometries([pcd, origin])
     print(pcd)
-    r = o3d.io.write_point_cloud("/home/alex/Projects/TRAIL/datasets/SCS_Sample/depth_img_converted.pcd", pcd)
+
+    converted_pcd_path = depth_img_path.replace('.png', '.pcd')
+    r = o3d.io.write_point_cloud(converted_pcd_path, pcd)
     if r:
-        print('written')
+        print("Converted PCD successfully written to:", converted_pcd_path)
     else:
-        print('could not write')
+        print("Could not write PCD")
 
 
 def compute_normals(pcd_path):
@@ -70,11 +54,11 @@ def compute_normals(pcd_path):
 
 
 if __name__ == "__main__":
-    input_depth = '/home/alex/Projects/TRAIL/cleargrasp/api/depth2depth/gaps/sample_files/input-depth.png'
-    zigzag_depth = '/home/alex/Projects/TRAIL/cleargrasp/api/depth2depth/gaps/sample_files/zigzag_depth_output.png'
-    # scaled_out_path = '/home/alex/Projects/TRAIL/cleargrasp/api/depth2depth/gaps/sample_files/correct_scaled.png'
-    # scale_depth_image(zigzag_depth, scaled_out_path, 32.0)
-    # create_point_cloud(zigzag_depth, 4.0)
+    parser = argparse.ArgumentParser(description='Convert Normals PCD to H5')
+    parser.add_argument('--png_computed', required=False, type=str, default='view_71_output.png', help='Name of .png file outputted by depth2depth')
+    args = parser.parse_args()
 
-    input_pcd_path = "/home/alex/Projects/TRAIL/datasets/SCS_Sample/zigzag_scene5_view0.pcd"
-    compute_normals(input_pcd_path)
+    input_dir = '/home/alex/Projects/TRAIL/depth-completion-solver/output_data/'
+    depth_img_path = input_dir + args.png_computed
+    
+    create_point_cloud(depth_img_path, scale=4.0)  # Take png_depth_scale used in depth2depth divide by 1000 to get scale here
